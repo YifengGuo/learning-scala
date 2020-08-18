13949790c16f26d371fcd4dbdd52d47d24442d64
YifengGuo <gyf940517@gmail.com>
2020-08-18 17:47:04 +0800

    function type

---
+++ src/main/scala/com/yifeng/scala/oop/high/order/types/FunctionType.scala
@@ -0,0 +1,24 @@
+package com.yifeng.scala.oop.high.order.types
+
+/**
+  * @author guoyifeng on 2020-08-18
+  */
+object FunctionType {
+
+  val max = (x: Int, y: Int) => if (x < y) y else x
+  val anonfun2 = new Function2[Int, Int, Int] {
+    def apply(x: Int, y: Int): Int = if (x < y) y else x
+  }
+
+  val succ = (x: Int) => x + 1
+  val anonfun1 = new Function1[Int, Int] {
+    def apply(x: Int): Int = x + 1
+  }
+
+  def main(args: Array[String]): Unit = {
+    // max与anonfun2是等价的，它们定义的都是输入参数是两个Int类型
+    // 返回值也是Int类型的函数。
+    assert(max(0, 1) == anonfun2(0, 1)) // true
+    assert(succ(0) == anonfun1(0)) // true
+  }
+}
